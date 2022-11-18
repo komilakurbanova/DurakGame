7317890aa03d7ed599dcdf030a1706015064ff72
Komila <113191657+komilakurbanova@users.noreply.github.com>
2022-11-18 21:41:18 +0300

    Update player.py
    Поправила уровни доступа

--- player.py
+++ player.py
@@ -6,25 +6,25 @@ CARDS_FOR_PLAYER = 6
 
 class Player(object):
     def __init__(self, puid: str, name: str, cards: List[card.Card]):
-        self._puid = puid
-        self._name = name
-        self._cards = cards
+        self.__puid = puid
+        self.name = name
+        self.__cards = cards
 
     def cards_quantity(self):
-        return len(self._cards)
+        return len(self.__cards)
 
     def puid(self):
-        return self._puid
+        return self.__puid
 
     def cards(self):
-        return self._cards
+        return self.__cards
 
     def __add_cards_from_deck(self, deck: List[card.Card], ind: int):
-        self._cards += deck[:ind]
+        self.__cards += deck[:ind]
         del deck[:ind]
 
     def take_cards_from_field(self, cards: List[card.Card]):  # брать со стола
-        self._cards += cards
+        self.__cards += cards
 
     def take_lack_cards_from_deck(self, deck: List[card.Card]):  # брать из колоды
         lack = max(0, CARDS_FOR_PLAYER - self.cards_quantity())
@@ -32,17 +32,14 @@ class Player(object):
         self.__add_cards_from_deck(deck, to)
 
     def remove_card(self, c: card.Card):
-        self._cards.remove(c)
+        self.__cards.remove(c)
 
     def sort_cards(self):  # возвращает словарь отсорченных по мастям карт (для реализации игры с ботом полезно)
         cards_by_suits = {}
         for s in card.Card.Suit:
             cards_by_suits[s] = []
-        for c in self._cards:
+        for c in self.__cards:
             cards_by_suits[c.suit].append(c.value)
         for s in card.Card.Suit:
             cards_by_suits[s].sort()
         return cards_by_suits
-
-
-
