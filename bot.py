4f672b6b52de2467a6138556eab3c836be253f10
Komila <113191657+komilakurbanova@users.noreply.github.com>
2022-12-15 01:03:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import logging
 import os
 import datetime as dt
 
+from telegram.ext import CallbackQueryHandler
+
 from game_alg import *
 
 logging.basicConfig(
@@ -45,8 +47,8 @@ def start_block(update: Update, context: CallbackContext) -> None:
     """
     username = update.message.from_user.username
 
-    check_user(update)
-    stage = get_stage(update)
+    check_user(update.message.chat_id, username)
+    stage = get_stage(username)
 
     update.message.reply_text(f"Привет, {username}!")
 
@@ -60,24 +62,34 @@ def main_block(update: Update, context: CallbackContext) -> None:
     1) Выдает результаты по кнопкам
     2) Реализует коннект двух пользователей через game_block
     """
+    flag_inline_card = check_inline_card(update)  # проверка на инлайн карtу
+    if flag_inline_card:
+        game_block(update.callback_query, context, flag_inline_card)
+        return
+    # print(update)
     username = update.message.from_user.username
     message = update.message.text
-    check_user(update)
-    stage = get_stage(update)
+    check_user(update.message.chat_id, username)
+    stage = get_stage(username)
 
     if stage == 'game':
-        game_block(update, context)
+        game_block(update, context, flag_inline_card)
         return
 
     if message == "Статистика":
         update.message.reply_text('Тут ничего нет)))', reply_markup=cancel_markup)
+        # TODO: где стата?
     elif message == "Правила":
         update.message.reply_text(help_text, reply_markup=cancel_markup)
+        # TODO: правила?
     elif message == "Назад":
-        update.message.reply_text("Назад", reply_markup=menu_markup)
-    elif message == "Игра с другом":
+        update.message.reply_text("Надеюсь, ты собой доволен", reply_markup=menu_markup)
+    elif message == "Игра":
         edit_stage(username, "wait_responce")
         update.message.reply_text("Пришли юзернейм друга")
+    elif message == "Изменить имя":
+        pass
+        # TODO: где?
 
     elif stage == 'wait_responce':
         username2 = message.split('@')[1]
@@ -86,19 +98,24 @@ def main_block(update: Update, context: CallbackContext) -> None:
         except DoesNotExist:
             update.message.reply_text("Твой друг никогда не заходил в бота")
             return
+        edit_stage(username, 'game')
+        edit_stage(username2, 'game')
         new_game(username, username2, context)
 
 
 def main() -> None:
-    updater = Updater(os.getenv('TG_TOKEN'))
+    updater = Updater('5590111815:AAGP4kaHOvck-THoO_zKGvfBuAX62DvRtGk')
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start_block))
+    dispatcher.add_handler(CallbackQueryHandler(main_block))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
                                           main_block))
-
-    updater.start_polling()
-    updater.idle()
+    try:
+        updater.start_polling()
+        updater.idle()
+    except:
+        os.remove('telegram.db')
 
 
 if __name__ == '__main__':
